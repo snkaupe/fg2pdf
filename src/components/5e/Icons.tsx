41ad719db85d64419adec7e01e481842783084b5
Sebastian N. Kaupe <sebastian.kaupe@t-online.de>
2025-05-02 13:18:20 +0000

    fix missing comma

--- src/components/5e/Icons.tsx
+++ src/components/5e/Icons.tsx
@@ -33,7 +33,7 @@ export const Icons: DnDIcons = {
 
     get spellComponents(): JSX.Element {
         return <svg  xmlns="http://www.w3.org/2000/svg"  width={ICON_SIZE}  height={ICON_SIZE}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width={STROKE_WIDTH}  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-components"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12l3 3l3 -3l-3 -3z" /><path d="M15 12l3 3l3 -3l-3 -3z" /><path d="M9 6l3 3l3 -3l-3 -3z" /><path d="M9 18l3 3l3 -3l-3 -3z" /></svg>;
-    }
+    },
 
     get spellRange(): JSX.Element {
         return <svg xmlns="http://www.w3.org/2000/svg" width={ICON_SIZE} height={ICON_SIZE} viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width={STROKE_WIDTH} stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-ruler-measure"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M19.875 12c.621 0 1.125 .512 1.125 1.143v5.714c0 .631 -.504 1.143 -1.125 1.143h-15.875a1 1 0 0 1 -1 -1v-5.857c0 -.631 .504 -1.143 1.125 -1.143h15.75z" /><path d="M9 12v2" /><path d="M6 12v3" /><path d="M12 12v3" /><path d="M18 12v3" /><path d="M15 12v2" /><path d="M3 3v4" /><path d="M3 5h18" /><path d="M21 3v4" /></svg>;
